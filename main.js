1df94b45e5c987ba2d25e18124f42a1e0a223045
Utri Gergely <utrig@yahoo.com>
2024-06-15 18:24:27 +0200

    főoldal értékek formázások nélkül

--- main.js
+++ main.js
@@ -29,31 +29,41 @@ function _load() {
       name: "Profit",
       val: "pro",
       mer: " $",
-      sum: true
+      sum: true,
+      prev: 0,
+      cur: 0
     },
     {
       name: "Polgár",
       val: "pop",
-      mer: "",
-      sum: true
+      mer: " fő",
+      sum: true,
+      prev: 0,
+      cur: 0
     },
     {
       name: "Nívó",
       val: "niv",
       mer: "%",
-      sum: false
+      sum: false,
+      prev: 0,
+      cur: 0
     },
     {
       name: "Öröm",
       val: "joy",
       mer: "%",
-      sum: false
+      sum: false,
+      prev: 0,
+      cur: 0
     },
     {
-      name: "Rend",
-      val: "def",
-      mer: " ×",
-      sum: true
+      name: "Erő",
+      val: "had",
+      mer: " &#9876;",
+      sum: true,
+      prev: 0,
+      cur: 0
     },
   ];
   window.selVal = "tax";
@@ -70,6 +80,7 @@ function _load() {
       joy: Math.floor(70 + Math.random() * 30),
       def: Math.floor(40 + Math.random() * 30),
       ufo: 10,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -79,7 +90,7 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: .7,
       defo: 1,
       culto: 1,
@@ -96,6 +107,7 @@ function _load() {
       joy: Math.floor(25 + Math.random() * 50),
       def: Math.floor(35 + Math.random() * 30),
       ufo: 5,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -105,7 +117,7 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: .7,
       defo: 1,
       culto: .7,
@@ -122,6 +134,7 @@ function _load() {
       joy: Math.floor(0 + Math.random() * 20),
       def: Math.floor(5 + Math.random() * 20),
       ufo: 0,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -131,7 +144,7 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: .7,
       defo: .7,
       culto: .7,
@@ -148,6 +161,7 @@ function _load() {
       joy: Math.floor(50 + Math.random() * 30),
       def: Math.floor(40 + Math.random() * 30),
       ufo: 0,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -157,7 +171,7 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: 1,
       defo: 1,
       culto: 1,
@@ -173,6 +187,7 @@ function _load() {
       joy: Math.floor(30 + Math.random() * 40),
       def: Math.floor(60 + Math.random() * 40),
       ufo: 3,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -182,7 +197,7 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: 1,
       defo: 1.3,
       culto: 1,
@@ -199,6 +214,7 @@ function _load() {
       joy: Math.floor(30 + Math.random() * 30),
       def: Math.floor(40 + Math.random() * 25),
       ufo: 0,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -208,7 +224,7 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: 1.3,
       defo: 1.3,
       culto: 1,
@@ -225,6 +241,7 @@ function _load() {
       joy: Math.floor(0 + Math.random() * 5),
       def: Math.floor(10 + Math.random() * 20),
       ufo: 0,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -234,7 +251,7 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: 1.3,
       defo: .7,
       culto: .7,
@@ -251,6 +268,7 @@ function _load() {
       joy: Math.floor(20 + Math.random() * 50),
       def: Math.floor(40 + Math.random() * 30),
       ufo: 0,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -260,7 +278,7 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: 1,
       defo: 1,
       culto: .7,
@@ -277,6 +295,7 @@ function _load() {
       joy: Math.floor(75 + Math.random() * 25),
       def: Math.floor(30 + Math.random() * 70),
       ufo: 0,
+      had: 0,
       pro: 0,
       popC: 0,
       mtnC: 0,
@@ -286,13 +305,32 @@ function _load() {
       hadC: 0,
       proC: 0,
       dev: [],
-      ev: [],
+      evs: [-1, -1],
       eco: 1.3,
       defo: 1,
       culto: 1.3,
     }
   ];
+
+  for (ko of ker) {
+    ko.pro = Math.round(ko.pop * (tax / 10) * (ko.niv / 100) - ko.mtn);
+    ko.had = Math.round((ko.def - ko.ufo * 10) * ko.defo);
+  };
+  for (let s = 1; s < selector.length; s++) {
+    let selo = selector[s];
+    let sum = 0;
+    for (k of ker) {
+      sum += k[selo.val];
+    }
+    if (!selector[s].sum) {
+      sum = Math.round(sum / 9);
+    }
+    selo.prev = sum;
+    selo.cur = sum;
+  }
+
   console.log(ker);
+  console.log(selector);
 
   //NEW DAY
   function newDay() {
@@ -300,24 +338,37 @@ function _load() {
     pageUpdate(true);
   }
 
-  function printMoney() {
-    if (money >= 1000000000) return Math.floor(money / 1000000000) + "B $";
-    if (money >= 1000000) return Math.floor(money / 1000000) + "M $";
-    if (money >= 2000) return money.toLocaleString() + " $";
-    if (money < 0) return "CSŐD!";
-    return money + " $";
+  function bigNumber(x, y) {
+    if (x >= 1000000000) return Math.floor(x / 1000000000) + "B " + y;
+    if (x >= 1000000) return Math.floor(x / 1000000) + "M " + y;
+    if (x >= 2000) return x.toLocaleString() + " " + y;
+    if (x < 0) {
+      switch (y) {
+        case "$":
+          return "CSŐD!";
+
+        case "fő":
+          return "0 fő";
+
+        default:
+          break;
+      }
+    }
+    return x + " " + y;
   }
 
   function generateXtra(so) {
+    let xtraStr = "";
     switch (so.val) {
       case "tax":
         let cc = tax < 20 ? 2 : tax > 50 ? 0 : 1;
-        let xtraStr = `
+        xtraStr = `
         <button class="navBtn" id="tMinus">-</button>
         <span id="tVal" style="color: ${bascol[cc]};">${tax} %</span>
         <button class="navBtn" id="tPlus">+</button>
         `;
         document.getElementById("extraInfo").innerHTML = xtraStr;
+
         let tm = document.getElementById("tMinus");
         let tp = document.getElementById("tPlus");
         if (tax < 1) tm.disabled = true;
@@ -337,12 +388,67 @@ function _load() {
           }
         });
 
+        for (let k = 0; k < 9; k++) {
+          let ei = ker[k].evs[0];
+          if (ei > -1) {
+            document.getElementById("ki1-" + k).innerHTML = `${ev[ei].name}`;
+          }
+          ei = ker[k].evs[1];
+          if (ei > -1) {
+            document.getElementById("ki2-" + k).innerHTML = `${ev[ei].name}`;
+          }
+        }
+
         break;
 
       default:
+        let chVal = so.cur - so.prev;
+        let chPer = Math.round((chVal / so.prev) * 100);
+        let chCol = chVal < 0 ? 0 : chVal > 0 ? 2 : 1;
+        let intro = "";
+        let chTxt = "";
+        if (so.sum) {
+          intro = "Összes";
+          if (chPer < 0) chTxt = chPer + " %";
+          if (chPer > 0) chTxt = "+" + chPer + " %";
+        } else {
+          intro = "Átlag";
+          if (chVal < 0) chTxt = chVal;
+          if (chVal > 0) chTxt = "+" + chVal;
+        }
+        xtraStr = `
+          <span class="infoVal">${intro}: ${bigNumber(so.cur, so.mer)}</span>
+          <span class="infoCh" style="color: ${bascol[chCol]}">${chTxt}</span>
+        `;
+        document.getElementById("extraInfo").innerHTML = xtraStr;
+
+        for (let k = 0; k < 9; k++) {
+          let val = ker[k][so.val];
+          let chV = ker[k][so.val + "C"];
+          let chT = "";
+          let chC = chV < 0 ? 0 : chV > 0 ? 2 : 1;
+          if (so.sum) {
+            if (chV < 0) chT = chV + " %";
+            if (chV > 0) chT = "+" + chV + " %";
+          } else {
+            if (chV < 0) chT = chV;
+            if (chV > 0) chT = "+" + chV;
+          }
+
+          document.getElementById("ki1-" + k).innerHTML = `${bigNumber(val, so.mer)}`;
+          document.getElementById("ki2-" + k).innerHTML = `${chT}`;
+          document.getElementById("ki2-" + k).style.color = bascol[chC];
+        }
+
         break;
     }
-    return xtraStr;
+  }
+
+  function changeSel(e) {
+    let sn = Number(e.target.id[1]);
+    selVal = selector[sn].val;
+    console.log(selVal);
+    pageUpdate(true);
   }
 
   function pageUpdate(foldal) {
@@ -352,7 +458,7 @@ function _load() {
       let pageStr = `
       <div id="header">
         <div id="topMenu">
-          <span class="navNr" title=${money.toLocaleString()} style="color: ${bascol[Number(money > 0)]};">${printMoney()}</span>
+          <span class="navNr" title=${money.toLocaleString()} style="color: ${bascol[Number(money > 0)]};">${bigNumber(money, "$")}</span>
           <img class="navBtn" id="saveBtn" src="./img/save.jpg">
           <img class="navBtn" id="loadBtn" src="./img/load.JPG">
           <img id='soundBtn' src=${musIcon} alt="music">
@@ -365,7 +471,7 @@ function _load() {
         `;
       for (let k = 0; k < 6; k++) {
         let selClass = selector[k].val === selVal ? "selected" : "unselected";
-        let selStr = `<button id="s${k}" class="navBtn ${selClass}">${selector[k].name}</button>`;
+        let selStr = `<button id="s${k}" class="navBtn selBtn ${selClass}">${selector[k].name}</button>`;
         pageStr += selStr;
       }
       pageStr += `</div>
@@ -373,11 +479,19 @@ function _load() {
       </div>
       `
       for (let k = 0; k < 9; k++) {
-        let kerStr = `<div id = "k${k}" class="ker"> ${ker[k].name}</div> `;
+        let kerStr = `<div id = "k${k}" class="ker">
+          <div class="kername">${ker[k].name}</div>
+          <div class="kerinfo1" id="ki1-${k}"></div>
+          <div class="kerinfo2" id="ki2-${k}"></div>
+        </div> `;
         pageStr += kerStr;
       }
       page.innerHTML = pageStr;
       generateXtra(selObj);
+
+      document.getElementById("endBtn").addEventListener("click", newDay);
+      document.querySelectorAll(".selBtn").forEach((s) => s.addEventListener("click", changeSel));
+
 
 
 
