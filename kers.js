f49ab6168b866cd9ab25046cf23ef947499cc2e6
Utri Gergely <utrig@yahoo.com>
2024-09-20 17:44:36 +0200

    helyek

--- kers.js
+++ kers.js
@@ -2,6 +2,7 @@ window.ker = [
   {
     num: 0,
     name: "Erdőszél",
+    hely: "az Erdőszélen",
     desc: "Egészséges, pihentető túrák, hőssegédek és bűvös lények otthona.",
     x: 1,
     y: 1,
@@ -117,6 +118,7 @@ window.ker = [
   {
     num: 1,
     name: "Farmok",
+    hely: "a Farmokon",
     desc: "Szorgos, egyszerű gazdálkodók földjei.",
     x: 2,
     y: 1,
@@ -232,6 +234,7 @@ window.ker = [
   {
     num: 2,
     name: "Ázsiai negyed",
+    hely: "az Ázsiai negyedben",
     desc: "Kismillió fura ember, akikről azt sem tudod, valójában mit csinálnak.",
     x: 3,
     y: 1,
@@ -318,6 +321,7 @@ window.ker = [
   {
     num: 3,
     name: "Városliget",
+    hely: "a Városligetben",
     desc: "Sportpályák, játszóterek, csónakázótó a kikapcsolódni vágyóknak.",
     x: 1,
     y: 2,
@@ -419,6 +423,7 @@ window.ker = [
   {
     num: 4,
     name: "Városközpont",
+    hely: "a Városközpontban",
     desc: "Itt van többek között a Városháza erődszerű épülete.",
     x: 2,
     y: 2,
@@ -548,6 +553,7 @@ window.ker = [
   {
     num: 5,
     name: "Üzleti negyed",
+    hely: "az Üzleti negyedben",
     desc: "A legnagyobb pénzintézetek és cégek központjainak felhőkarcolói magasodnak itt.",
     x: 3,
     y: 2,
@@ -663,6 +669,7 @@ window.ker = [
   {
     num: 6,
     name: "Ipartelep",
+    hely: "az Ipartelepen",
     desc: "Működő és bezárt gyárak, munkásszállók nyomasztó rengetege.",
     x: 1,
     y: 3,
@@ -776,6 +783,7 @@ window.ker = [
   {
     num: 7,
     name: "Lakónegyed",
+    hely: "a Lakónegyedben",
     desc: "Sok polgár lakhelye sok lehetőséggel. Itt van a Branyiszkó-tér is.",
     x: 2,
     y: 3,
@@ -812,10 +820,84 @@ window.ker = [
     defo: 1,
     culto: .7,
     szar: "pop_<_1500",
+    vélemény: [
+      {
+        cond: "niv_<_30",
+        duma: [
+          "Nem lehet megélni itt ennyi nyugdíjból, kérem!",
+          "Ágyámá egy kis pízt! Há visszáádom hónap! Ne csináld mán!",
+          "",
+          "Egy munkatábor az egész lepra hely. Ha elvégeztem volna a 8 általánost, elhúznék innét a valagba!",
+          "Az üzemi büfé bezárt, de nincs egy rendes bolt sem a környéken! Nem tudunk enni!",
+          "Ha valaki beteg lesz, nincs pénze orvosra sem! Megdöglik, azt ennyi vót.",
+          "Azt mondják, lehet, hogy bezár az gyár. Akkor mit fogunk csinálni? Maga fogja etetni a gyerekeket?",
+          "Alig tudjuk már eladni, amit gyártunk, a felére kellett visszavennünk a termelést!",
+        ]
+      },
+      {
+        cond: "had_<_10",
+        duma: ["Aszongyák, az éjjelesek közül hármat fura lények raboltak el az utcán! Nagyobb biztonság kellene!",
+          "Lopják az alkatrészeket! Nem tudom, kik, de ezek nem a mieink, az biztos!",
+          "Múltkor eltűnt egy egész marógép. Nem az ajtón vitték ki, a plafonon volt egy luk! Hallott már ilyet?",
+          "Valaki folyamatosan lop a készletekből! Azt mondta a Józsi, hogy ufók viszik el. Mire kellhet nekik hat tonna acéllemez?",
+          "Látja ezt a hatalmas lyukat a falon? Maga szerint mivel lehet ilyet csinálni? Nem vagyunk biztonságban!",
+          "Mityu szerint beépültek közénk az idegenek. Mármint ufók. Mindig is gyanús volt nekem a Mari, szemüveg nélkül heggeszt...",
+          "Rendszeresek az üsszetűzések a kohászok meg a bányászok között! Rendet kellene tenni! Kevesek ehhez a gépészek!"
+        ]
+      },
+      {
+        cond: "pro_<_1500",
+        duma: [
+          "Múlt héten újabb üzem zárt be! Nem lesz ez így jó...",
+          "Kezd teljesen tönkremenni ez a kerület! Miért hagyja, ha maga a polgármester? Csináljon valamit!",
+          "Megmondom őszintén: elfogyott a pénz. Ha nincs beruházás, nem tudunk termelni.",
+          "Nemcsak a beszállítók kifizetését, hanem már a működési költségeket sem tudjukkigazdálkosni! Tönkre fogunk menni...",
+          "Muszáj zsebre dolgoznunk! Nem a mi hibánk...",
+          "Nézzed meg ezt a rengeteg számlát! Nem az adó itt a gond, azt leszarjuk, anélkül még jobban is működünk. De ezeket ki kell fizetni, hogy ne húzzuk le a rolót!"
+        ]
+      },
+      {
+        cond: "pop_<_1200",
+        duma: [
+          "Nagyon fogy a munkaerő! Már mindenki három műszakban dolgozik egyszerre! Nem állhatunk le!",
+          "Ilyen kevés emberrel nem tudunk gazdaságosn üzemelni. A gépeket is kezelni kell valakinek!",
+          "Én a műszakvezető vagyok, de kénytelen voltam én is beállni lapátolni mióta a Lacit elvitte a köszvény!",
+          "Azért van ekkora kupleráj, mert kénytelenek voltunk a személyügyes Icukát a targoncához ültetni. Volt 1 perce megtanulni a kezelést... Nincs ember!",
+          "Fogd meg inkább ezt a rohadt kart ahelyett, hogy itt traccsolsz, és ha szólok, húzd meg! Nincs más ember! Nem azt...! ÁÁÁÁÁÁÁÁÁÁÁ!",
+          "Már mindenki bezárt, csak mi vagyunk. Annyira nincs ember! Még szerencse, hogy búgócsiga mindig kell az embereknek..."
+        ]
+      },
+      {
+        cond: "pro_>_3000",
+        duma: [
+          "Fantasztikusan megy az üzlet! E hónapban újabb termékekkel bővült a kínálatunk!",
+          "Már nemcsak vasrugókat gyártunk, hanem vasmacskákat is! Apám, ha még élne, büszke lenne rám!",
+          "Fontolgatjuk, hogy újra beindítjuk a régi fagyigyárat. Kipofozzuk egy kicsit és újra lesz itt minden!",
+          "Újabb gyártósort tudtunk venni! Megduplázzuk a termelést!",
+          "Le tudunk mi mindent gyártani, ami kell. Csak mondja, hogy mi kell, és megoldjuk!",
+          "Ki hitte volna, hogy ennyi ember fog volfrámpörölyt venni? Már külföldre is szállítunk!"
+        ]
+      },
+    ],
+    semmi: [
+      "Mi itt dolgozunk, kérem. Nem érünk rá beszélgetni!",
+      "Nem értem... Már harmadszor vágok le belőle, oszt még mindig rövid!",
+      "Állj hátrébb, ha nem akarsz szénné égni!",
+      "BRRRRRRRRRR, KSSSSSSSSSSSSSSSS, Nyihunyihunyihu... Mi van?! BRRRRRRRRRRRRRR! Nem értem!",
+      "Hozni kell a tervet, különben ordítani fog a főnök! Nem tudunk leállni!",
+      "Hogy miért nincs védőfelszerelése Gazsinak? Ő csak feketézik, valakinek a haverja. Bejelenteni? Azt se tudjuk, Gazsi-e az igazi neve.",
+      "Tudjuk, hogy koszos a munkásszálló, de ez kinek a hibája, he?",
+      "Milyen bűz? Azóta van más szag, hogy te bejöttél!",
+      "Bekötjük mi a villanyórát a városházán úgy, hogy nem lesz gond vele. Megcsináljuk olcsón okosba!",
+      "Nem kell a városházának vashokedli? Rajtunk maradt 600 hokedli, mert túl korán csődbe ment a megrendelő.",
+      "A raklap meg a járólap között annyi a különbség, mint a... Teszed le azt a döngölő békát, amikor hozzád beszélek! Na, itt a vége a gyárlátogatásnak!",
+      "Gyere ki onnan, héééé...! Már megint egy beszorult hulla miatt kell leállítani az egész gyártósort, hogy dolgozzon így az ember?!"
+    ]
   },
   {
     num: 8,
     name: "Kultúrnegyed",
+    hely: "a Kultúrnegyedben",
     desc: "Színvonalas kultúrintézmények pezsgő éjszakai élettel.",
     x: 3,
     y: 3,
